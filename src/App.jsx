00b1e7b1238f8632913666adc775ef230f236318
Your Name <you@example.com>
2023-12-14 23:22:12 -0300

    BreakPoints Images BackGround

--- src/App.jsx
+++ src/App.jsx
@@ -1,86 +1,87 @@
-import Header from './components/Header'
-import TodoCreate from './components/TodoCreate'
-import TodoComputed from './components/TodoComputed'
-import TodoFilter from './components/TodoFilter'
-import TodoList from './components/TodoList'
-import { useEffect, useState } from 'react'
+import Header from "./components/Header";
+import TodoCreate from "./components/TodoCreate";
+import TodoComputed from "./components/TodoComputed";
+import TodoFilter from "./components/TodoFilter";
+import TodoList from "./components/TodoList";
+import { useEffect, useState } from "react";
 
-const initialStateTodos = [
-]
+const initialStateTodos = JSON.parse(localStorage.getItem('todos')) || [];
 
 const App = () => {
-  const [todos, setTodos] = useState(initialStateTodos)
-  useEffect(() =>{
-    localStorage.setItem('todos', JSON.stringify(todos));
-  }, [todos])
-  const createTodo = (title) => {
-    const newTodo = {
-      id: Date.now(),
-      title: title.trim(),
-      completed: false,
-    }
-    setTodos([...todos, newTodo])
-  }
+    const [todos, setTodos] = useState(initialStateTodos);
+    useEffect(() => {
+        localStorage.setItem("todos", JSON.stringify(todos));
+    }, [todos]);
+    const createTodo = (title) => {
+        const newTodo = {
+            id: Date.now(),
+            title: title.trim(),
+            completed: false,
+        };
+        setTodos([...todos, newTodo]);
+    };
 
-  const removeTodo = (id) => {
-    setTodos(todos.filter((todo) => todo.id !== id))
-  }
+    const removeTodo = (id) => {
+        setTodos(todos.filter((todo) => todo.id !== id));
+    };
 
-  const updateTodo = (id) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo,
-      ),
-    )
-  }
+    const updateTodo = (id) => {
+        setTodos(
+            todos.map((todo) =>
+                todo.id === id ? { ...todo, completed: !todo.completed } : todo
+            )
+        );
+    };
 
-  const computedItemsLeft = todos.filter((todo) => !todo.completed).length
+    const computedItemsLeft = todos.filter((todo) => !todo.completed).length;
 
-  const clearCompleted = () => {
-    setTodos(todos.filter((todo) => !todo.completed))
-  }
+    const clearCompleted = () => {
+        setTodos(todos.filter((todo) => !todo.completed));
+    };
 
-  const [filter, setFilter] = useState('all')
+    const [filter, setFilter] = useState("all");
 
-  const changeFilter = (filter) => setFilter(filter);
+    const changeFilter = (filter) => setFilter(filter);
 
-  const filtersTodo = () => {
-    switch (filter) {
-      case 'all':
-        return todos
-      case 'active':
-        return todos.filter((todo) => !todo.completed)
-      case 'completed':
-        return todos.filter((todo) => todo.completed)
+    const filtersTodo = () => {
+        switch (filter) {
+            case "all":
+                return todos;
+            case "active":
+                return todos.filter((todo) => !todo.completed);
+            case "completed":
+                return todos.filter((todo) => todo.completed);
 
-      default:
-        return todos
-    }
-  }
+            default:
+                return todos;
+        }
+    };
 
-  return (
-    <div className="transition-all duration-1000 bg-contain bg-no-repeat bg-[url('./assets/images/bg-mobile-light.jpg')] dark:bg-[url('./assets/images/bg-mobile-dark.jpg')] bg-gray-300 min-h-screen dark:bg-gray-950">
-      <Header />
+    return (
+        <div className="min-h-screen bg-gray-300 
+        bg-[url('./assets/images/bg-mobile-light.jpg')] md:bg-[url('./assets/images/bg-desktop-light.jpg')] bg-contain bg-no-repeat transition-all duration-1000 
+        dark:bg-gray-950 dark:bg-[url('./assets/images/bg-mobile-dark.jpg')] dark:md:bg-[url('./assets/images/bg-desktop-dark.jpg')]">
+            <Header />
 
-      <main className="container mx-auto px-4 mt-8">
-        <TodoCreate createTodo={createTodo} />
-        <TodoList
-          todos={filtersTodo()}
-          removeTodo={removeTodo}
-          updateTodo={updateTodo}
-        />
-        <TodoComputed
-          computedItemsLeft={computedItemsLeft}
-          clearCompleted={clearCompleted}
-        />
-        <TodoFilter changeFilter={changeFilter} filter={filter}/>
-      </main>
+            <main className="container mx-auto mt-8 px-4">
+                <TodoCreate createTodo={createTodo} />
+                <TodoList
+                    todos={filtersTodo()}
+                    removeTodo={removeTodo}
+                    updateTodo={updateTodo}
+                />
+                <TodoComputed
+                    computedItemsLeft={computedItemsLeft}
+                    clearCompleted={clearCompleted}
+                />
+                <TodoFilter changeFilter={changeFilter} filter={filter} />
+            </main>
 
-      <footer className="transition-all duration-1000 text-center mt-10 dark:text-gray-500">
-        Created by Gustavo Díaz
-      </footer>
-    </div>
-  )
-}
+            <footer className="mt-10 text-center transition-all duration-1000 dark:text-gray-500">
+                Created by Gustavo Díaz
+            </footer>
+        </div>
+    );
+};
 
-export default App
+export default App;
